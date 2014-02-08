ac1fcfa29d220ce3822c5c07df07500839436f00
John D. Storey <johndangerstorey@gmail.com>
2014-02-07 22:44:08 -0700

    adds xoisland project and a lot of deletable munter files

--- xoisland/assets/js/main.js
+++ xoisland/assets/js/main.js
@@ -7,5 +7,26 @@
 				$('#theMenu').toggleClass('menu-open');
 			});
 
+			// Smooth Scrolling w/o any other plugins
+			$(function() {
+			  $('a[href*=#]:not([href=#])').click(function() {
+			    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			      var target = $(this.hash);
+			      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			      if (target.length) {
+			        $('html,body').animate({
+			          scrollTop: target.offset().top
+			        }, 1000);
+			        return false;
+			      }
+			    }
+			  });
+			});
+
+			// Toggles FAQ answers
+			$( "#toggleAnswer" ).click(function(e) {
+				e.preventDefault();
+  			$( 'p' ).toggleClass( "toggleThis" );
+});
 
 })(jQuery)
